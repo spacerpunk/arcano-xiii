41783895a5289587117511b69e73fc04e4f650f3
Nicolas Requena <58339988+NRequena@users.noreply.github.com>
2024-10-21 19:01:28 -0300

    arcano-3

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import Contact from './Components/Contact';
 const App = () => {
   return (
     <div className="overflow-x-hidden text-neutral-700 antialiased
-    selection:bg-cyan-300 selection:text-cyan-900">
+    selection:bg-red-500 selection:text-neutral-00">
       <div className="fixed top-0 -z-10 h-full w-full">
       <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]"></div>
       </div>
